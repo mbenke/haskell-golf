0c664a658746855d871f2098c459aa159271a32d
Marcin Benke <marcin.benke@gmail.com>
2019-04-23 11:56:40 +0200

    List.prodWith

--- src/Golf/List.hs
+++ src/Golf/List.hs
@@ -4,5 +4,10 @@ module Golf.List where
 
 prod :: [a] -> [b] -> [(a,b)]
 -- prod xs ys = [(x,y) | x <- xs, y <- ys]
-prod (x:xs) ys = map (x,) ys ++ prod xs ys
-prod _ _ = []
+-- prod (x:xs) ys = map (x,) ys ++ prod xs ys
+-- prod _ _ = []
+prod = prodWith (,)
+
+prodWith :: (a->b->c) -> [a] -> [b] -> [c]
+prodWith f (x:xs) ys = map (f x) ys ++ prodWith f xs ys
+prodWith f [] _ = []

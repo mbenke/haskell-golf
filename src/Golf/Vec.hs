e770c52af96c8e15effb2938e03b6f7e7c9d4497
Marcin Benke <marcin.benke@gmail.com>
2019-05-28 14:26:41 +0200

    naive vappend

--- src/Golf/Vec.hs
+++ src/Golf/Vec.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE Rank2Types          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-
+{-# LANGUAGE TypeOperators #-}
 
 module Golf.Vec where
 import Golf.Nat
@@ -18,3 +18,7 @@ vhead (x:>_) = x
 
 vtail :: Vec a (S n) -> Vec a n
 vtail (_:>xs) = xs
+
+vappend :: Vec a m -> Vec a n -> Vec a (m:+n)
+vappend V0 ys = ys
+vappend (x:>xs) ys = x:>vappend xs ys
